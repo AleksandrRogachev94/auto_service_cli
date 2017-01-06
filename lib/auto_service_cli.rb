47812af24705082f7eae15b98096908bc2fa2d43
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-01-06 06:53:54 -0500

    add creating of URL and validation

--- lib/auto_service_cli.rb
+++ lib/auto_service_cli.rb
@@ -3,6 +3,16 @@ require "nokogiri"
 require "pry"
 
 require_relative "auto_service_cli/version"
+require_relative "auto_service_cli/constants"
+require_relative "auto_service_cli/service_center"
+require_relative "auto_service_cli/scraper"
+
+# Modules
 
 module AutoServiceCLI
 end
+
+# Errors
+
+class InvalidURLData < StandardError; end
+class InvalidURL < StandardError; end
--- lib/auto_service_cli/scraper.rb
+++ lib/auto_service_cli/scraper.rb
@@ -1,12 +1,23 @@
 class AutoServiceCLI::Scraper
-  attr_reader :url, :doc, :sort_type
+  attr_reader :doc, :sort_type, :zip
 
-  def initialize(year)
-    sort_type = "default"
-    @url = "#{AutoServiceCLI::URL_WORLD_RANKIGS_TEMPLATE}#{year}"
-    @doc = Nokogiri::HTML(open("http://www.topuniversities.com/university-rankings/university-subject-rankings/2016/engineering-chemical"))
+  def initialize(zip)
+    @sort_type = "default"
+    @zip = zip
     binding.pry
-    #year = Date.today.strftime("%Y%m")
+    #@url = "#{AutoServiceCLI::URL_WORLD_RANKIGS_TEMPLATE}#{year}"
+    #@doc = Nokogiri::HTML(open("http://www.topuniversities.com/university-rankings/university-subject-rankings/2016/engineering-chemical"))
+    #binding.pry
+  end
+
+  def get_url
+    raise InvalidURLData, "Invalid input data (zip or sort type)!!!" unless valid_url_data?
+    AutoServiceCLI::URL_TEMPLATE << "#{@zip}&s=#{sort_type}"
+    #http://www.yellowpages.com/search?search_terms=auto%20service&geo_location_terms=17401&s=default
+  end
+
+  def valid_url_data?
+    (sort_type == "default" || sort_type == "distance" || sort_type == "rating" || sort_type == "name") && zip > 0
   end
 
   def scrape_centers
@@ -14,5 +25,3 @@ class AutoServiceCLI::Scraper
     universities = self.doc.css(".uni a").text
   end
 end
-
-http://www.yellowpages.com/search?search_terms=auto%20service&geo_location_terms=17401&s=default
